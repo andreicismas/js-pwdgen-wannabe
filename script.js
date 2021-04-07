512890d2d54fc5d1d844ce77f2b73e10cbe3795a
andreicismas <andrei_cismas@yahoo.it>
2021-04-07 17:05:05 +0200

    js project/start & end

---
+++ script.js
@@ -0,0 +1,19 @@
+var myName =prompt("Il Tuo Nome ");
+console.log( "The First Name " + myName)  ;
+          
+var secondName =prompt("Il Tuo Cognome");
+console.log(  "second Name Is"+ secondName);
+
+var color =prompt("Il Tuo Colore");
+console.log( "Favorit Color Is"+ color); 
+
+document.getElementById("my_name").innerHTML= "The Name Is: " + myName ;
+
+document.getElementById("second_name").innerHTML= "The Second Name Is: " + secondName ;
+
+document.getElementById("color").innerHTML= "The Favorit Color Is: " + color ;
+
+document.getElementById("number_21").innerHTML= "The Favorit Number Is: " + " " + "21" ;
+
+
+
